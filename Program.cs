b15fa30a911c1701194ad6fe7df6475d349938b4
elinapavlova <65790951+elinapavlova@users.noreply.github.com>
2021-07-10 21:56:51 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    class Program
+    {
+        /// <summary>
+        /// Функция для битового реверса массива байтов
+        /// </summary>
+        /// <param name="array"></param>
+        public static void Reverse(byte[] array)
+        {
+            foreach (byte element in array)
+            {
+                string tmp = String.Format("{0:d8}", Convert.ToInt32(Convert.ToString(element, 2)));
+
+                string reverseTmp = new string(tmp.ToCharArray().Reverse().ToArray());
+
+                int reverseElement = Convert.ToInt32(reverseTmp, 2);
+
+                Console.WriteLine(reverseElement);
+            }
+        }
+
+        static async Task Main(string[] args)
+        {
+            // Проверка работы класса UserRepository
+           
+            var user = await UserRepository.GetUserAsync(1);
+
+            Console.WriteLine(user);
+
+            UserRepository.AddUserAsync(5, "John");
+
+            await UserRepository.GetOrderedUsersAsync();
+
+
+            Console.WriteLine();
+
+            // Проверка работы функции Reverse
+
+            byte[] array = { 0, 10, 50, 100, 255 };
+
+            Reverse(array);
+        }
+    }
+}
---
+++ UserRepository.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    public class UserRepository
+    {
+        /// <summary>
+        /// Хранилище данных
+        /// </summary>
+        static Dictionary<int, string> users = new Dictionary<int, string>
+        {
+            [0] = "Alex",
+            [2] = "Ann",
+            [1] = "Kate"
+        };
+
+        /// <summary>
+        /// Добавление пользователя в хранилище
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="userName"></param>
+        public static void AddUser(int userId, string userName)
+        {
+            users.Add(userId, userName);
+        }
+
+        /// <summary>
+        /// Получение пользователя по UserId
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        public static string GetUser(int userId)
+        {
+            return users[userId];
+        }
+
+        /// <summary>
+        /// Получение отсортированного по UserId списка пользователей
+        /// </summary>
+        /// <returns></returns>
+        public static SortedDictionary<int, string> GetOrderedUsers()
+        {
+            var sortedUsers = new SortedDictionary<int, string>(users);
+
+            foreach (var a in sortedUsers)
+            {
+                Console.WriteLine(a.Key + " - " + a.Value);
+            }
+
+            return sortedUsers;
+        }
+
+        /// <summary>
+        ///  Асинхронная версия метода AddUser()
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="userName"></param>
+        public static async void AddUserAsync(int userId, string userName)
+        {
+            await Task.Run(() => AddUser(userId, userName));
+        }
+
+        /// <summary>
+        /// Асинхронная версия метода GetUser()
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        public static async Task<string> GetUserAsync(int userId)
+        {
+            return await Task.Run(() => GetUser(userId));
+        }
+        
+        /// <summary>
+        /// Асинхронная версия метода GetOrderedUsers()
+        /// </summary>
+        /// <returns></returns>
+        public static async Task<SortedDictionary<int, string>> GetOrderedUsersAsync()
+        {
+            return await Task.Run(() => GetOrderedUsers());
+        }
+    }
+}
